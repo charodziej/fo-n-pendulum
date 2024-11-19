8ec6e86c979dabd97610feb6afa9c09f10e09875
Adam Jeliński <adas.jelinski@gmail.com>
2024-11-19 21:12:42 +0100

    style: run prettier

--- src/stores/pendulum.js
+++ src/stores/pendulum.js
@@ -51,52 +51,100 @@ export const usePendulumStore = defineStore('pendulum', () => {
     )
 
     const aMatrix = (phis) => {
-        let matrix = [];
+        let matrix = []
         for (let i = 0; i < links.value; i++) {
-            let row = [];
+            let row = []
             for (let j = 0; j < links.value; j++) {
-                row.push((links.value - Math.max(i, j) + 1) * Math.cos(phis[i] - phis[j]));
+                row.push(
+                    (links.value - Math.max(i, j) + 1) *
+                        Math.cos(phis[i] - phis[j])
+                )
             }
             matrix.push(row)
         }
-        return matrix;
+        return matrix
     }
 
     const rightVector = (phis, phiDots) => {
-        let vector = [];
+        let vector = []
         for (let i = 0; i < links.value; i++) {
-            let vec_i = 0;
+            let vec_i = 0
             for (let j = 0; j < links.value; j++) {
-                vec_i -= (links.value - Math.max(i, j) + 1) * Math.sin(phis[i] - phis[j]) * phiDots[j] ** 2;
+                vec_i -=
+                    (links.value - Math.max(i, j) + 1) *
+                    Math.sin(phis[i] - phis[j]) *
+                    phiDots[j] ** 2
             }
-            vec_i -= gravConstant.value * (links.value - i + 1) * Math.sin(phis[i]);
-            vector.push(vec_i);
+            vec_i -=
+                gravConstant.value * (links.value - i + 1) * Math.sin(phis[i])
+            vector.push(vec_i)
         }
-        return vector;
+        return vector
     }
 
     const solveEquationsOfMotion = (phis, phiDots) => {
-        let A = aMatrix(phis);
-        let b = rightVector(phis, phiDots);
-        return [phiDots, math.lusolve(A, b).map(x => x[0])];
+        let A = aMatrix(phis)
+        let b = rightVector(phis, phiDots)
+        return [phiDots, math.lusolve(A, b).map((x) => x[0])]
     }
 
     const RK4 = (phis, phiDots, timeDelta) => {
-        let k1 = solveEquationsOfMotion(phis, phiDots);
-        let k2 = solveEquationsOfMotion(math.add(phis, k1[0].map(x => 0.5*timeDelta*x)), math.add(phiDots, k1[1].map(x => 0.5*timeDelta*x)));
-        let k3 = solveEquationsOfMotion(math.add(phis, k2[0].map(x => 0.5*timeDelta*x)), math.add(phiDots, k2[1].map(x => 0.5*timeDelta*x)));
-        let k4 = solveEquationsOfMotion(math.add(phis, k3[0].map(x => 1.0*timeDelta*x)), math.add(phiDots, k3[1].map(x => 1.0*timeDelta*x)));
-    
-        let phiDeltas    = math.add(k1[0], k2[0].map(x => 2 * x), k3[0].map(x => 2 * x), k4[0]).map(x => x * timeDelta/6);
-        let phiDotDeltas = math.add(k1[1], k2[1].map(x => 2 * x), k3[1].map(x => 2 * x), k4[1]).map(x => x * timeDelta/6);
-    
+        let k1 = solveEquationsOfMotion(phis, phiDots)
+        let k2 = solveEquationsOfMotion(
+            math.add(
+                phis,
+                k1[0].map((x) => 0.5 * timeDelta * x)
+            ),
+            math.add(
+                phiDots,
+                k1[1].map((x) => 0.5 * timeDelta * x)
+            )
+        )
+        let k3 = solveEquationsOfMotion(
+            math.add(
+                phis,
+                k2[0].map((x) => 0.5 * timeDelta * x)
+            ),
+            math.add(
+                phiDots,
+                k2[1].map((x) => 0.5 * timeDelta * x)
+            )
+        )
+        let k4 = solveEquationsOfMotion(
+            math.add(
+                phis,
+                k3[0].map((x) => 1.0 * timeDelta * x)
+            ),
+            math.add(
+                phiDots,
+                k3[1].map((x) => 1.0 * timeDelta * x)
+            )
+        )
+
+        let phiDeltas = math
+            .add(
+                k1[0],
+                k2[0].map((x) => 2 * x),
+                k3[0].map((x) => 2 * x),
+                k4[0]
+            )
+            .map((x) => (x * timeDelta) / 6)
+        let phiDotDeltas = math
+            .add(
+                k1[1],
+                k2[1].map((x) => 2 * x),
+                k3[1].map((x) => 2 * x),
+                k4[1]
+            )
+            .map((x) => (x * timeDelta) / 6)
+
         return [math.add(phis, phiDeltas), math.add(phiDots, phiDotDeltas)]
     }
 
     const simulationTick = (timeDelta) => {
-        let newValues = RK4(angles.value, angularVelocities.value, timeDelta);
-        angles.value = newValues[0];
-        angularVelocities.value = newValues[1];
+        let newValues = RK4(angles.value, angularVelocities.value, timeDelta)
+        angles.value = newValues[0]
+        angularVelocities.value = newValues[1]
     }
 
     const tick = (delta) => {
